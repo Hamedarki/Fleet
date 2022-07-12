955f128065ea2492da725a8e639f93d10279e2a9
hamed Arki <hamedarki1379@gmail.com>
2022-07-12 12:18:35 +0430

    responsive home page done:mobile & tablet

--- JS/app.js
+++
@@ -1,6 +0,0 @@
-const bar = document.querySelector(".fa-bars");
-const menu = document.querySelector(".item-hide");
-
-bar.addEventListener("click", () => {
-    menu.classList.toggle("show-menu");
-});
